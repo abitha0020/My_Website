c328e9e5f1ca62d692a83c8524d606f6ddb939ce
abitha0020 <132005925+abitha0020@users.noreply.github.com>
2024-08-12 23:00:35 +0530

    achievements session

--- src/components/achievements.tsx
+++ src/components/achievements.tsx
@@ -6,39 +6,46 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
-import achievementData from "../Datas/achievements.json"; 
+import achievementData from "../Datas/achievements.json";
+import Title from "./ui/title"; 
 
-export default function AchievementsCarousel() {
+export default function Achievements() {
   return (
-    <div className="relative w-full max-w-5xl mx-auto py-8">
-      <Carousel
-        opts={{
-          align: "start",
-        }}
-        className="w-full"
-      >
-        <CarouselContent>
-          {achievementData.achievements.map((achievement, index) => (
-            <CarouselItem key={index} className="basis-full sm:basis-1/2 ">
-              <Card className="h-full">
-                <img
-                  src={achievement.image}
-                  alt={achievement.title}
-                  className="w-full h-48 object-cover rounded-t-md"
-                />
-                <CardContent className="p-4">
-                  <h3 className="text-lg font-semibold">{achievement.title}</h3>
-                  <p className="text-sm text-muted-foreground">
-                    {achievement.description}
-                  </p>
-                </CardContent>
-              </Card>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-        <CarouselPrevious className="absolute left-2 top-1/2 transform -translate-y-1/2 sm:left-4 z-10" />
-        <CarouselNext className="absolute right-2 top-1/2 transform -translate-y-1/2 sm:right-4 z-10" />
-      </Carousel>
+    <div>
+        <div className="container">
+            <Title title="ACHIEVEMENTS"  />
+        </div>    
+        <div className="relative w-full max-w-5xl mx-auto py-8">
+        <Carousel
+            opts={{
+            align: "start",
+            }}
+            className="w-full"
+        >
+            <CarouselContent>
+            {achievementData.achievements.map((achievement, index) => (
+                <CarouselItem key={index} className="basis-full sm:basis-1/2 ">
+                <Card className="h-full">
+                    <img
+                    src={achievement.image}
+                    alt={achievement.title}
+                    className="w-full h-64 object-cover rounded-t-md"
+                    />
+                    <CardContent className="p-4">
+                    <h3 className="text-lg font-semibold">{achievement.title}</h3>
+                    <p className="text-sm text-muted-foreground">
+                        {achievement.description}
+                    </p>
+                    </CardContent>
+                </Card>
+                </CarouselItem>
+            ))}
+            </CarouselContent>
+            <CarouselPrevious className="absolute left-2 top-1/2 transform -translate-y-1/2 sm:left-4 z-10" />
+            <CarouselNext className="absolute right-2 top-1/2 transform -translate-y-1/2 sm:right-4 z-10" />
+        </Carousel>
+        </div>
     </div>
+    
   );
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,7 +2,7 @@ import Navbar from "@/components/navbar";
 import About from "@/components/about";
 import Projects from "@/components/projects";
 import Experience from "@/components/experience";
-import AchievementsCarousel from "@/components/achievements";
+import Achievements from "@/components/achievements";
 export default function Home(){
     return(
         <div className="bg-black">
@@ -10,7 +10,7 @@ export default function Home(){
             <About />
             <Projects />
             <Experience />
-            <AchievementsCarousel />
+            <Achievements />
         </div>
     );
 }
